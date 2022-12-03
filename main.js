00d934ec258a06a3e5af4a97edd728fc14911294
TresorRw <alaintresorcyusa683@gmail.com>
2022-12-03 22:48:56 +0200

    added: logout

--- main.js
+++ main.js
@@ -33,17 +33,24 @@ loginBtn.addEventListener('click', (e) => {
 });
 
 // Login function
+// admin username: master ..password: master
 let login = ()=> {
     if (name1.value == '' || pwd.value == '') {
         console.log("please fill");
     } else {
-        for (let person of account) {
-            if (person.username == name1.value && person.pass == pwd.value) {
-                document.cookie = `user= ${person.username}; expires= Dec 20 2022 00:00:00`;
-                window.location.href = 'dashboard.html';
-                break;
-            } else {
-                console.log('not found');
+        // Loging master as the admin
+        if (name1.value == 'master' && pwd.value == 'master') {
+            document.cookie = `admin=master; exprires=Dec 25 2030 00:00:00; path='/'`;
+            window.location.href = 'dashboard.html';
+        } else { // If user is not admin, search into our database
+            for (let person of account) {
+                if (person.username == name1.value && person.pass == pwd.value) {
+                    document.cookie = `user= ${person.username}; expires= Dec 20 2022 00:00:00`;
+                    window.location.href = 'user.html';
+                    break;
+                } else {
+                    console.log('not found');
+                }
             }
         }
     }
